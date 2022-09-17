2439f2dcc81e56cc04d82b8fb520a896b5ade595
BetsySC <113737380+BetsySC@users.noreply.github.com>
2022-09-16 19:16:40 -0500

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,1 @@
+const words = ['Gato','Perro', 'Serpiente','Elefante','Cangrejo','Pulpo','Araña','Condor','Puma','Ballena','Caracol','Abeja','Tigre','Zorro','Llama','Mono','Oso','Colibri','Rana','Cuy'];
